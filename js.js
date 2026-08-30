aaaa10082a8fc1a71f44e2cc59eb8fa4d051e9c0
Ali Farid <54527194+AliFarid7@users.noreply.github.com>
2026-08-30 21:22:56 +0300

    Finish first 2 steps

---
+++ js.js
@@ -0,0 +1,49 @@
+const myLibrary = [
+];
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+    }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("1984", "George Orwell", 328, false);
+addBookToLibrary("No Country for Old Men", "Cormac McCarthy", 309, true);
+
+function displayBooks() {
+    myLibrary.forEach(function(book) {
+        const myBooksContainer = document.querySelector(".Bookcontainer")
+        const div = document.createElement("div");
+        div.classList.add("book-card");
+        // div.textContent = book.info()
+        const p1 = document.createElement("p");
+        p1.textContent = book.title;
+        div.appendChild(p1);
+        const p2 = document.createElement("p");
+        p2.textContent = book.author;
+        div.appendChild(p2);
+        const p3 = document.createElement("p");
+        p3.textContent = book.pages;
+        div.appendChild(p3);
+        const p4 = document.createElement("p");
+        p4.textContent = book.read;
+        div.appendChild(p4);
+        myBooksContainer.appendChild(div);
+    });
+}
+
+displayBooks();
